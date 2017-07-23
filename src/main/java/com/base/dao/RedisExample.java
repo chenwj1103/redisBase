c5e3c41577f14b388a74ae1bdcac4a69829a3270
陈伟杰 <chenwj3@ifeng.com>
2017-07-23 20:26:59 +0800

    调整redis数据的键的长度

--- src/main/java/com/base/dao/RedisExample.java
+++ src/main/java/com/base/dao/RedisExample.java
@@ -9,27 +9,27 @@ public class RedisExample extends RedisClient {
     public static void test() {
 
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 5000000; i++) {
+        for (int i = 0; i < 10000; i++) {
             double d =Math.random();
             int num = (int)(d*100000000);
 
             for (int j=0 ;j<100;j++){
                 num++;
-                String StringKey = "num:" + num;
+                String StringKey = "testKeyLength++++++++++++++++++++++++++++++++++num:" + num;
                 set(StringKey, num+"");
                 System.out.println("StringKey======="+StringKey);
             }
 
             for (int j=0 ;j<100;j++){
                 num++;
-                String listKey ="listKey:"+num;
+                String listKey ="testKeyLength++++++++++++++++++++++++++++++++++listKey:"+num;
                 lPush(listKey,num+"");
                 System.out.println("listKey======="+listKey);
             }
 
             for (int j=0 ;j<100;j++){
                 num++;
-                String mapKey ="mapKey:"+num;
+                String mapKey ="testKeyLength++++++++++++++++++++++++++++++++++mapKey:"+num;
                 hSet(mapKey,"test",num+"");
                 System.out.println("mapKey======="+mapKey);
             }
@@ -37,7 +37,7 @@ public class RedisExample extends RedisClient {
 
             for (int j=0 ;j<100;j++){
                 num++;
-                String setKey ="setKey:"+num;
+                String setKey ="testKeyLength++++++++++++++++++++++++++++++++++setKey:"+num;
                 sAdd(setKey,num+"");
                 System.out.println("setKey======="+setKey);
             }
@@ -45,7 +45,7 @@ public class RedisExample extends RedisClient {
 
             for (int j=0 ;j<100;j++){
                 num++;
-                String zaddKey ="zaddKey:"+num;
+                String zaddKey ="testKeyLength++++++++++++++++++++++++++++++++++zaddKey:"+num;
                 zAdd(zaddKey,Math.random(),num+"");
                 System.out.println("zaddKey======="+zaddKey);
             }
